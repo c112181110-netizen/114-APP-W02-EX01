15f04c5b4390c5ed83ff1ed291824375338d764a
c112181110-netizen <c112181110@nkust.edu.tw>
2026-03-05 11:27:30 +0800

    114-APP-W02-EX01

--- helloworld.java
+++
@@ -1,8 +0,0 @@
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
-        System.out.println("helloworld");
-        int grade = 80;
-        if (grade >=60) {System.out.println("You can pass\n" + grade );}
-    }
-}
